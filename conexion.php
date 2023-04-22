3e2d17dd7abeb5258c32c4153f75f963e99a174b
Manuel <mriverospolo@gmail.com>
2023-04-21 23:27:29 -0500

    v2

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 	function conexion(){
-	$host = "host=containers-us-west-41.railway.app";
-	$port = "port=6953";
+	$host = "host=containers-us-west-160.railway.app";
+	$port = "port=5937";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "password=gx48a5ALGCFUHSn4bfGb";
+	$password = "password=VOblkTXDxfqYPcDsrayE";
 	$db = pg_connect("$host $port $dbname $user $password");
     return $db;
 }
